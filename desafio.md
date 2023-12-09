cb0f6aa25192888f8de3a1b7529e50f50cae4620
José Alexandre <92656216+JAlexandreLS@users.noreply.github.com>
2023-12-09 00:01:32 -0300

    Commit de arquivos adicionados ao projeto e modificações nos arquivos já existentes

---
+++ desafio.md
@@ -0,0 +1,21 @@
+# Desafio do Classificador de Nivel de herói. #
+
+## Esse é um desafio do Bootcamp da DIO/iFood. ##
+
+>Consiste em criar um site com o que aprendemos até aqui de HTML5, muito pouco de CSS e o básico do JavaScript.
+>
+>Apanhar o nome de um Herói e sua quantidade de experiência(XP).
+>
+>Se XP for menor do que 1.000 = Ferro
+>Se XP for entre 1.001 e 2.000 = Bronze
+>Se XP for entre 2.001 e 5.000 = Prata
+>Se XP for entre 5.001 e 7.000 = Ouro
+>Se XP for entre 7.001 e 8.000 = Platina
+>Se XP for entre 8.001 e 9.000 = Ascendente
+>Se XP for entre 9.001 e 10.000 = Imortal
+>Se XP for maior ou igual a 10.001 = Radiante 
+>
+>No final, passar uma mensagem:
+>
+>O Herói de nome **{nome}** está no nível de **{nível}**
+
--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-let nomeHeroi = getElementById(heroi)
 
-console.log(nomeHeroi)
+function verificar() {
+var nomeDig = document.querySelector('input#name')
+var valorXp = document.querySelector('input#numeroXp')
+var res = document.querySelector('div#res')
+var heroi = valorXp.value
+
+     
+if (heroi <= 1000) {
+    
+        res.innerHTML = `O Herói chamado de ${nomeDig.value} está classificado em <strong>Ferro</strong>.`
+                        
+} else if (heroi >= 1001 && heroi <= 2000) {
+        res.innerHTML = `O Herói chamado de ${nomeDig.value} está classificado em <strong>Bronze</strong>.`
+          
+
+} else if (heroi >= 2001 && heroi <= 5000) {
+        res.innerHTML = `O Herói chamado de ${nomeDig.value} está classificado em <strong>Prata</strong>.`
+          
+    
+} else if (heroi >= 5001 && heroi <= 7000) {
+        res.innerHTML = `O Herói chamado de ${nomeDig.value} está classificado em <strong>Ouro</strong>.`
+          
+
+} else if (heroi >= 7001 && heroi <= 8000) {
+        res.innerHTML = `O Herói chamado de ${nomeDig.value} está classificado em <strong>Platina</strong>.`
+              
+
+} else if (heroi >= 8001  && heroi <= 9000) {
+        res.innerHTML = `O Herói chamado de ${nomeDig.value} está classificado em <strong>Ascendente</strong>.`
+          
+
+} else if (heroi >= 9001 && heroi <= 10000) {
+        res.innerHTML = `O Herói chamado de ${nomeDig.value} está classificado em <strong>Imortal</strong>.`
+          
+
+} else {
+        res.innerHTML = `O Herói chamado de ${nomeDig.value} está classificado em <strong>Radiante</strong>.`
+          
+}                        
+   
+}
